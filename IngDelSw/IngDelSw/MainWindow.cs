20ebc5e5e6364781920ca94260ed0a2f24d50a90
ItsMeeLuigi <34719513+ItsMeeLuigi@users.noreply.github.com>
2018-05-16 10:57:21 +0200

    GTK# is gut

---
+++ IngDelSw/IngDelSw/MainWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	//private Label labelDiProva2;
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+		Label labelDiProva = new Label ("Questo è un label di prova.");
+		//Button bottoneDiProva = new Button ("bottone");
+		this.Add (labelDiProva);
+		this.ShowAll ();
+		this.DeleteEvent += new DeleteEventHandler (OnDeleteEvent);
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+}
